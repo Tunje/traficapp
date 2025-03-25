4d5131eeda3d761269355accf324814c56bbe5de
Roz Townsend <rdtownsend@protonmail.com>
2025-03-25 12:36:56 +0100

    remove old API files

--- client/src/components/config.example.ts
+++
@@ -1,5 +0,0 @@
-/*----- Example config file: Note now that all API keys are stored in the server as an environment variable with dotenv -----*/
-
-// export const WEATHER_API_KEY = "your_openweather_api_key_here";
-// export const GOOGLE_MAPS_API_KEY = "your_google_maps_api_key_here";
-// export const TRAFIKVERKET_API_KEY = `your_trafikverket_api_key_here`;
