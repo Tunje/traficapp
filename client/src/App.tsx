f9d62eb1661c5fda5c393106d4807b272efa2f33
Sweodin <petergustafsson910@gmail.com>
2025-04-07 08:15:34 +0200

    fixed headline

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,15 +16,16 @@ const App: React.FC = () => {
 
   const getLocation = async (address: string): Promise<Coordinates> => {
     try {
-      const response = await fetch(`http://localhost:3000/api/location?address=${address}`
+      const response = await fetch(
+        `http://localhost:3000/api/location?address=${address}`
       );
-      
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-      
+
       const data = await response.json();
-      
+
       if (data.latitude && data.longitude) {
         const coords = { latitude: data.latitude, longitude: data.longitude };
         setCoordinates(coords);
@@ -40,7 +41,6 @@ const App: React.FC = () => {
       return null;
     }
   };
-  
 
   const handleSearch = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -68,62 +68,62 @@ const App: React.FC = () => {
       <div className="logo-container">
         <img src={logoImage} alt="Logo" className="logo" />
         <div className="logo-container__title">
-          <h1>TrafficJam</h1>
+          <h1>Trafik & Väder översikt</h1>
         </div>
       </div>
 
       <section className="search">
         <div className="search-container">
-        <form onSubmit={handleSearch}>
-          <div className="search-bar-container">
-            <input
-              type="text"
-              className="search-input"
-              placeholder="Sök plats"
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-            />
-            <button type="submit" className="search-button">
-              Sök
-            </button>
-          </div>
-        </form>
+          <form onSubmit={handleSearch}>
+            <div className="search-bar-container">
+              <input
+                type="text"
+                className="search-input"
+                placeholder="Sök plats"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <button type="submit" className="search-button">
+                Sök
+              </button>
+            </div>
+          </form>
         </div>
       </section>
-      
+
       {!loading && coordinates && (
         <>
-        <section className="search-results">
-        <div className="results-container">
-          <h3>Sök resultat</h3>
-          {results.length > 0 ? (
-            <ul className="results-list">
-              {results.map((result, index) => (
-                <li key={index} className="result-item">
-                  {result}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <p className="no-results">Inga resultat</p>
-          )}
-          </div>
-      </section>
+          <section className="search-results">
+            <div className="results-container">
+              <h3>Sök resultat</h3>
+              {results.length > 0 ? (
+                <ul className="results-list">
+                  {results.map((result, index) => (
+                    <li key={index} className="result-item">
+                      {result}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p className="no-results">Inga resultat</p>
+              )}
+            </div>
+          </section>
 
-        {/* Left side - Transport departures */}
-        <section className="transport-departures">
-          <DepartureInfo />
-        </section>
+          {/* Left side - Transport departures */}
+          <section className="transport-departures">
+            <DepartureInfo />
+          </section>
 
-        {/* Right side - Weather */}
-        <section className="weather-updates">
-          <Weather coordinates={coordinates} />
-        </section>
+          {/* Right side - Weather */}
+          <section className="weather-updates">
+            <Weather coordinates={coordinates} />
+          </section>
 
-        {/* Bottom - Traffic Situation Updates */}
-      <section className="traffic-updates">
-        <TrafficInfo />
-        </section>
+          {/* Bottom - Traffic Situation Updates */}
+          <section className="traffic-updates">
+            <TrafficInfo />
+          </section>
         </>
       )}
     </main>
